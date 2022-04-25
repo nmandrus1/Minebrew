39ecd364588d916be751545a1bd40378010bf511
nmandrus1 <nmandrus1@gmail.com>
2022-04-24 22:50:19 -0400

    Set up basic project structure and Config.toml

--- target/debug/deps/minebrew-dcf8a074987ecc5c.d
+++
@@ -1,5 +0,0 @@
-/Users/jmanabc/projects/Minebrew/target/debug/deps/minebrew-dcf8a074987ecc5c: src/main.rs
-
-/Users/jmanabc/projects/Minebrew/target/debug/deps/minebrew-dcf8a074987ecc5c.d: src/main.rs
-
-src/main.rs:
--- target/debug/minebrew.d
+++
@@ -1,1 +0,0 @@
-/Users/jmanabc/projects/Minebrew/target/debug/minebrew: /Users/jmanabc/projects/Minebrew/src/main.rs
