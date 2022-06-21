8aee407f515448288efa20508fae7567f0c087af
nmandrus1 <nmandrus1@gmail.com>
2022-06-21 15:55:19 -0400

    changes to bin.rs in master

--- src/bin/bin.rs
+++ src/bin/bin.rs
@@ -1,23 +1,28 @@
-use mbrew_lib::{traits::*, modrinth::{Search, SearchResult}};
+use mbrew_lib::{
+    modrinth::{Search, SearchResult},
+    traits::*,
+};
 
 fn main() {
-    let v = vec![1, 2, 3].iter();
     let query = std::env::args().nth(1).unwrap().to_lowercase();
     let version = "1.18.2";
     let s = Search::new(&query, version);
 
     let res = s.search().unwrap();
 
-    if let Some(s) = res.iter()
-        .find(|s| s.slug() == query || s.title().to_lowercase() == query) {
-            println!("{} id: {}", s.slug(), s.id())
+    if let Some(s) = res
+        .iter()
+        .find(|s| s.slug() == query || s.title().to_lowercase() == query)
+    {
+        println!("{} id: {}", s.slug(), s.id())
     } else {
 
     }
 
-    let results: Vec<&SearchResult> = res.iter()
+    let results: Vec<&SearchResult> = res
+        .iter()
         .filter(|s| s.slug().contains(&query) || s.title().to_lowercase().contains(&query))
-        .collect(); 
+        .collect();
 
     if results.is_empty() {
         eprintln!("target not found: \"{}\" with version: {}", query, version);
